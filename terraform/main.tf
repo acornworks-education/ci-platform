496fc7bd62ca2aeac6bcd5626f38c856fd86c0dc
JeongSuk Lee <acornworks.kr@gmail.com>
2023-01-05 16:17:52 +1100

    Add a workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,36 @@ resource "aws_s3_bucket" "terraform-state" {
       "org" = "acornworks"
     }
 }
+
+resource "aws_db_instance" "test_db" {
+    allocated_storage = 50    
+    identifier = "testdb"
+    storage_type = "gp2"
+    engine = "postgresql"
+    instance_class = "db.t2.medium"
+    username = "admin"
+    password = "P@ssw0rd"
+    publicly_accessible = true
+    db_subnet_group_name = aws_db_subnet_group.db-subnet.name
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "private-db-subnet-se2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "ap-southeast-2"
+}
+
+resource "aws_subnet" "private-db-subnet-ne2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "ap-northeast-2"
+}
+
+resource "aws_db_subnet_group" "db-subnet" {
+    name = "db_subnet_group"
+    subnet_ids = [aws_subnet.private-db-subnet-se2.id, aws_subnet.private-db-subnet-ne2.id]
+}
